8c4333fccc6eefded2c5cd7dcf74fab7b8f06375
erickillian <erickillian39@gmail.com>
2024-10-21 22:59:23 -0700

    updates UI for extra wordle rankings card to have profile picture

--- frontend/src/components/wordle/ExtraWordleRankingsCard.vue
+++ frontend/src/components/wordle/ExtraWordleRankingsCard.vue
@@ -1,7 +1,10 @@
 <template>
     <v-card max-width="400">
-        <v-card-title>
-            {{ wordle?.user?.display_name ? wordle.user.display_name : 'Unknown User' }}
+        <v-card-title style="text-align: center; display: flex; flex-direction: column; align-items: center;">
+            <v-avatar size="64" class="mx-auto mb-2">
+            <img v-if="wordle?.user?.profile_picture" :src="getProfilePictureUrl(wordle.user.profile_picture)" alt="Profile Picture" width="64" height="64">
+            </v-avatar>
+            <div>{{ wordle?.user?.display_name ? wordle.user.display_name : 'Unknown User' }}</div>
         </v-card-title>
         <v-card-subtitle style="margin-bottom: 10px;">
             <div style="display: flex; flex-direction: column; gap: 10px;">
@@ -47,6 +50,9 @@ export default {
         formatDate(dateString) {
             const options = { year: 'numeric', month: 'long', day: 'numeric' };
             return new Date(dateString).toLocaleDateString(undefined, options);
+        },
+        getProfilePictureUrl(picture_url) {
+            return `/static/profile_pictures/${picture_url}`;
         }
     },
 };
