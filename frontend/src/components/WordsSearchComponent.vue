a1e3755d1b006e6fd7c9aa697df3e8c5e20b0e1a
erickillian <erickillian39@gmail.com>
2024-10-22 22:14:05 -0700

    Updates words search component

--- frontend/src/components/WordsSearchComponent.vue
+++ frontend/src/components/WordsSearchComponent.vue
@@ -1,51 +1,22 @@
 <template>
-    <v-card class="pa-4" style="overflow-y: auto;" elevation="0">
-        <v-card-title>
-            <v-text-field v-model="searchQuery" append-icon="mdi-magnify" label="Search Words" dense outlined
-                single-line hide-details />
-        </v-card-title>
-        <v-card-text>
-            <v-alert v-if="error" type="error" dismissible>
-                Error Loading Words
-            </v-alert>
-            <v-data-table-server v-else :headers="headers" :items="items" :items-length="items.length" item-key="word"
-                dense class="elevation-0" :loading="loading" hover @click:row="selectWord" hide-default-footer>
-                <template v-slot:item="{ item }">
-                    <tr :class="{ 'highlighted-row': selectedWord === item.word }" @click="() => selectWord(item)"
-                        style="cursor: pointer;">
-                        <td>{{ item.word }}</td>
-                    </tr>
-                </template>
-                <template v-slot:bottom>
-                    <div class="d-flex justify-center py-4 align-center">
-                        <v-pagination v-model="page.current_page" :length="page.total_pages" :total-visible="5"
-                            size="small" @update:model-value="onPageUpdate" />
-                    </div>
-                </template>
-                <template v-slot:no-data>
-                    <div class="pa-4">
-                        <v-alert :value="true" icon="mdi-information">
-                            No words found.
-                        </v-alert>
-                    </div>
-                </template>
-            </v-data-table-server>
-        </v-card-text>
-    </v-card>
+    <SearchTable :headers="headers" :items="items" :search-query="searchQuery" item-key="word" item-display-field="word"
+        search-label="Search Words" search-icon="mdi-magnify" item-name-plural="words" :selected-item="selectedWord"
+        :error="error" :loading="loading" :page="page" @item-selected="selectWord" @update:page="onPageUpdate"
+        @update:search-query="onSearchQueryUpdate" />
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { usePaginatedSearch } from "@/composables/usePaginatedSearch";
-import { words as fetchWords } from "@/api/wordle";
+import { defineComponent } from 'vue';
+import { usePaginatedSearch } from '@/composables/usePaginatedSearch';
+import { words as fetchWords } from '@/api/wordle';
+import SearchTable from '@/components/SearchTable.vue';
 
 export default defineComponent({
-    name: "WordsSearchComponent",
+    name: 'WordsSearchComponent',
     props: {
         selectedWord: {
             type: String,
-            required: false,
-            default: null,
+            required: true,
         },
         onWordSelected: {
             type: Function,
@@ -53,42 +24,30 @@ export default defineComponent({
         },
     },
     setup(props) {
-        const {
-            searchQuery,
-            items,
-            loading,
-            error,
-            page,
-            onPageUpdate,
-        } = usePaginatedSearch(fetchWords);
+        const { searchQuery, items, loading, error, page, onPageUpdate } = usePaginatedSearch(fetchWords);
 
-        const headers = [
-            { title: "Word", value: "word" },
-        ];
+        const headers = [{ title: 'Word', value: 'word' }];
 
-        const selectWord = (item) => {
-            const word = item.word;
-            const newWord = (props.selectedWord === word) ? null : word;
+        const selectWord = (word) => {
+            const newWord = props.selectedWord === word ? null : word;
             props.onWordSelected?.(newWord);
         };
 
+        const onSearchQueryUpdate = (newQuery) => {
+            searchQuery.value = newQuery;
+        };
+
         return {
-            searchQuery,
+            headers,
             items,
             loading,
             error,
             page,
             selectWord,
-            headers,
             onPageUpdate,
+            searchQuery,
+            onSearchQueryUpdate,
         };
     },
 });
 </script>
-
-<style scoped>
-.highlighted-row {
-    background-color: #ffffff33;
-    transition: background-color 0.3s ease-in-out;
-}
-</style>
