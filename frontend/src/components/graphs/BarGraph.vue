48a100d5d88a9cee7a53b5bd2775f8ef38316b35
erickillian <erickillian39@gmail.com>
2024-10-24 20:53:22 -0700

    Fixes bar graph issues with re-sizing

--- frontend/src/components/graphs/BarGraph.vue
+++ frontend/src/components/graphs/BarGraph.vue
@@ -1,20 +1,19 @@
 <template>
     <div class="bar-graph-container">
-        <Bar :data="barGraphData" :options="barGraphOptions" />
+        <canvas ref="barGraphCanvas"></canvas>
     </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import { defineComponent, computed } from 'vue'
-import { useTheme } from 'vuetify'
+import { Chart as ChartJS, BarController, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import { defineComponent, onMounted, onUnmounted, ref, computed, watch } from 'vue';
+import { useTheme } from 'vuetify';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+// Register all required components, including BarController
+ChartJS.register(BarController, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default defineComponent({
     name: 'BarGraph',
-    components: { Bar },
     props: {
         keys: {
             type: Array,
@@ -27,6 +26,9 @@ export default defineComponent({
     },
     setup(props) {
         const theme = useTheme();
+        const barGraphCanvas = ref(null);
+        let barChartInstance = null;
+
         const barGraphData = computed(() => ({
             labels: props.keys,
             datasets: [{
@@ -40,7 +42,7 @@ export default defineComponent({
             responsive: true,
             maintainAspectRatio: false,
             animation: {
-                duration: 400 // Disable animation
+                duration: 400
             },
             plugins: {
                 legend: { display: false }
@@ -51,13 +53,50 @@ export default defineComponent({
             }
         };
 
-        return { barGraphData, barGraphOptions }
+        onMounted(() => {
+            if (barGraphCanvas.value) {
+                barChartInstance = new ChartJS(barGraphCanvas.value, {
+                    type: 'bar',
+                    data: barGraphData.value,
+                    options: barGraphOptions
+                });
+            }
+        });
+
+        watch([() => props.keys, () => props.values], () => {
+            if (barChartInstance) {
+                barChartInstance.data.labels = props.keys;
+                barChartInstance.data.datasets[0].data = props.values;
+                barChartInstance.update(); // Update the chart to reflect the new data
+            }
+        });
+
+        onUnmounted(() => {
+            if (barChartInstance) {
+                barChartInstance.destroy();
+            }
+        });
+
+        return { barGraphCanvas }
     }
-})
+});
 </script>
 
 <style scoped>
 .bar-graph-container {
+    width: 100%;
+    height: auto;
+    position: relative;
+    padding-bottom: 50%;
+    /* Adjust this value to maintain the aspect ratio */
+    max-height: 500px;
+    /* Example max-height, adjust as necessary */
+}
+
+.bar-graph-container canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
 }
